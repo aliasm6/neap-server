b0e3d3650809f7b5d241f690585ecb5c3e3a15d2
Alias M6 <aliasmontoya719@gmail.com>
2016-10-24 11:00:35 -0600

    updated route coffee.js

--- src/server/routes/coffee.js
+++ src/server/routes/coffee.js
@@ -17,4 +17,18 @@ router.get('/', (req, res, next) => {
   });
 });
 
+router.get('/:id', (req, res, next) => {
+  const coffeeId = parseInt(req.params.id)
+  return knex('coffee').where('id', coffeeId).first()
+  .then((coffees) => {
+    res.status(200).json({
+      status: 'success',
+      data: coffees
+    });
+  })
+  .catch((err) => {
+    return next(err);
+  });
+});
+
 module.exports = router;
